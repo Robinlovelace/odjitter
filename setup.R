74e8d0114af7622a286c1c47d98dc7065d83a68c
Robin Lovelace <rob00x@gmail.com>
2021-05-11 08:57:46 +0100

    Set up foraper repo

---
+++ setup.R
@@ -0,0 +1,16 @@
+# Aim: This script documents the steps to set up this repo for reproducibility
+# and sharing of knowledge/know-how
+
+# The repo was created on github with the following command in the system CLI
+# (requires the github cli tool)
+# cd papers
+# gh repo create odjitter
+
+# Open RStudio and create a project as follows
+rstudioapi::openProject("~/papers/odjitter")
+
+# Set-up with usethis
+usethis::use_readme_rmd()
+unlink(".git/hooks/pre-commit")
+# Edit the paper/readme
+file.edit("README.Rmd")
